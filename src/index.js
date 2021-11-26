e43e6258ca2307b05ce5a7cf4650cea33a31ced2
mttcug <mttcug@163.com>
2021-11-26 15:05:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,43 @@ import create from './lib/create/index'
 
 const [ explain, cliCommand, command, projectName ] = process.argv
 
-program.command('create')
-        .option('-c', 'create a new project')
-        .action(() => {
+const commands = {
+    'create': {
+        description: 'create a new project',
+        usage: [
+            'prx create <project-name>'
+        ],
+        action: () => {
             return create(projectName)
-        })
+        }
+    },
+    'page': {
+        description: 'create a new page',
+        usage: [
+            'prx page <page-name>'
+        ],
+        action: () => {
+            return page(projectName)
+        }
+    }
+}
+
+Object.keys(commands).map((command) => {
+    program.command(command)
+            .description(commandMap[command].description)
+            .usage(commandMap[command].usage[0])
+            .action(() => {
+                commandMap[command].action()
+            })
+})
+
+
+// function help () {
+//     Object.keys(commands).map((command) => {
+//         console.log('-'+command, commandMap[command].usage[0], commandMap[command].description)
+//     })
+// }
+
+// program.on('-h', help)
 
 program.parse(process.argv)
